ac251b0e2048eba73253a607af9702d8896d03e6
paulo <paulocardoso98@gmail.com>
2020-12-02 12:37:07 +0000

    ros?

--- part8/p8_ex4/src/publisher.py
+++ part8/p8_ex4/src/publisher.py
@@ -4,7 +4,8 @@ import argparse
 import rospy
 from std_msgs.msg import String
 from colorama import Fore
-from dog_lib import Dog
+# from dog_lib import Dog
+from p8_ex4.msg import Dog
 
 
 def main():
@@ -15,17 +16,23 @@ def main():
     parser.add_argument('-f', '--frequency', type=float, default=10, help='frequency of publications (Hz)')
     args = vars(parser.parse_args())
 
-    pub = rospy.Publisher(args['topic_name'], String, queue_size=10)
+    pub = rospy.Publisher(args['topic_name'], Dog, queue_size=10)
     rospy.init_node('publisher', anonymous=True)
     rate = rospy.Rate(args['frequency'])
 
-    dog = Dog(name='Bobi', age=7, color='black')
-    dog.addBrother('Lassie')
+    #dog = Dog(name='Bobi', age=7, color='black')
+    #dog.addBrother('Lassie')
+
+    dog_msg = Dog()
+    dog_msg.name = 'Bobi'
+    dog_msg.age = 7
+    dog_msg.color = 'Black'
+    dog_msg.brothers.append('Lassie')
 
     while not rospy.is_shutdown():
-        rospy.loginfo('Publishing messege: ' + Fore.RED + args['message_content'] + Fore.RESET
+        rospy.loginfo('Publishing messege: ' + Fore.RED + str(dog_msg) + Fore.RESET
                       + '" on topic ' + args['topic_name'])
-        pub.publish(args['message_content'])
+        pub.publish(dog_msg)
         rate.sleep()
 
 
--- part8/p8_ex4/src/subscriber.py
+++ part8/p8_ex4/src/subscriber.py
@@ -4,10 +4,11 @@ import argparse
 from colorama import Fore
 import rospy
 from std_msgs.msg import String
+from p8_ex4.msg import Dog
 
 
 def messageRecivedCallback(message):
-    rospy.loginfo('recived message: "' + Fore.RED + message.data + Fore.RESET + '"')
+    rospy.loginfo('recived message: "' + Fore.RED + str(message) + Fore.RESET + '"')
 
 
 def main():
@@ -16,7 +17,7 @@ def main():
     args = vars(parser.parse_args())
 
     rospy.init_node('Subscriber', anonymous=True)
-    rospy. Subscriber(args['topic_name'], String, messageRecivedCallback)
+    rospy. Subscriber(args['topic_name'], Dog, messageRecivedCallback)
 
     rospy.spin()
 
